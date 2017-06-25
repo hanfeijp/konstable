27df46025d676f793c2ac53d7dee5348973fb2f1
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2017-06-25 10:23:30 -0400

    Add basic setup scripts

---
+++ konstable/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '0.0.0'
+import argparse
+import os
+import sys
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Konstable command line interface')
+    parser.add_argument('-v', '--version', dest='version', action='store_true',
+                        help="""Current Konstable Version""")
+
+    return parser.parse_args()
+
+def main():
+    print('Konstable running in ', os.getcwd())
+    args = create_parser()
+    if args.version:
+        print('Version:', __version__)
+        sys.exit()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from konstable import __version__
+
+setup(
+    name='konstable',
+    version=__version__,
+    description='A Python project.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    url='https://github.com/zafarali/konstable',
+    author='Zafarali Ahmed',
+    author_email='zafarali.ahmed@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+    python_requires='>=3.4.3',
+    entry_points={
+        'console_scripts':[
+            'konstable=konstable:main'
+        ]
+    }
+)
